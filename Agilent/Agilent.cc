be692cd3ee03195508c627d980fce06fd1bc8612
Norton Allen <allen@huarp.harvard.edu>
2015-08-27 13:36:10 -0700

    drv, not devno

--- Agilent/Agilent.cc
+++ Agilent/Agilent.cc
@@ -14,26 +14,26 @@ const unsigned TwisTorr::TT_DevNo[N_TWISTORR_DRIVES] = {1};
 
 void enqueue_polls(TwisTorr *TT, TwisTorr_t *TT_TM) {
   for (unsigned drv = 0; drv < N_TWISTORR_DRIVES; ++drv) {
-    unsigned devno = TwisTorr::TT_DevNo[drv];
-    TT->enqueue_poll_float(devno, 200, &(TT_TM->drive[drv].pump_current));
-    TT->enqueue_poll_float(devno, 201, &(TT_TM->drive[drv].pump_voltage));
-    TT->enqueue_poll_float(devno, 202, &(TT_TM->drive[drv].pump_power));
-    TT->enqueue_poll_float(devno, 203, &(TT_TM->drive[drv].driving_freq));
-    TT->enqueue_poll_float(devno, 204, &(TT_TM->drive[drv].pump_temperature));
-    TT->enqueue_poll_float(devno, 205, &(TT_TM->drive[drv].pump_status));
-    TT->enqueue_poll_float(devno, 210, &(TT_TM->drive[drv].rotation_speed));
-    TT->enqueue_poll_bit(devno, 0, &(TT_TM->drive[drv].flags), 0x01);
-    TT->enqueue_poll_bit(devno, 1, &(TT_TM->drive[drv].flags), 0x02);
-    TT->enqueue_poll_bit(devno, 106, &(TT_TM->drive[drv].flags), 0x04);
-    TT->enqueue_poll_bit(devno, 107, &(TT_TM->drive[drv].flags), 0x08);
-    TT->enqueue_poll_bit(devno, 122, &(TT_TM->drive[drv].flags), 0x10);
-    TT->enqueue_poll_bit(devno, 123, &(TT_TM->drive[drv].flags), 0x20);
+    // unsigned drv = TwisTorr::TT_DevNo[drv];
+    TT->enqueue_poll_float(drv, 200, &(TT_TM->drive[drv].pump_current));
+    TT->enqueue_poll_float(drv, 201, &(TT_TM->drive[drv].pump_voltage));
+    TT->enqueue_poll_float(drv, 202, &(TT_TM->drive[drv].pump_power));
+    TT->enqueue_poll_float(drv, 203, &(TT_TM->drive[drv].driving_freq));
+    TT->enqueue_poll_float(drv, 204, &(TT_TM->drive[drv].pump_temperature));
+    TT->enqueue_poll_float(drv, 205, &(TT_TM->drive[drv].pump_status));
+    TT->enqueue_poll_float(drv, 210, &(TT_TM->drive[drv].rotation_speed));
+    TT->enqueue_poll_bit(drv, 0, &(TT_TM->drive[drv].flags), 0x01);
+    TT->enqueue_poll_bit(drv, 1, &(TT_TM->drive[drv].flags), 0x02);
+    TT->enqueue_poll_bit(drv, 106, &(TT_TM->drive[drv].flags), 0x04);
+    TT->enqueue_poll_bit(drv, 107, &(TT_TM->drive[drv].flags), 0x08);
+    TT->enqueue_poll_bit(drv, 122, &(TT_TM->drive[drv].flags), 0x10);
+    TT->enqueue_poll_bit(drv, 125, &(TT_TM->drive[drv].flags), 0x20);
   }
 }
 
 int main(int argc, char **argv) {
   oui_init_options(argc, argv);
-  nl_error( 0, "Starting V0.1.2" );
+  nl_error( 0, "Starting V0.1.5" );
   { Selector Loop;
     TwisTorr_t TT_TM;
     TwisTorr TT(agilent_path);
