c7df8d664d9e42179d7a46ab435ac3840fc5c285
Norton Allen <allen@huarp.harvard.edu>
2015-08-27 11:13:09 -0700

    Possible SIGSEGV

--- Agilent/TwisTorr_cmd.cc
+++ Agilent/TwisTorr_cmd.cc
@@ -46,7 +46,7 @@ int TwisTorr_cmd::ProcessData(int flag) {
           }
         } else {
           int i;
-          for (i = 0; buf[cp+i] != '\0' && buf[cp+i] != '\r' && buf[cp+i] != '\n'; ++i);
+          for (i = 0; cp+i < nc && buf[cp+i] != '\0' && buf[cp+i] != '\r' && buf[cp+i] != '\n'; ++i);
           buf[cp+i] = '\0';
           command_request *cr = TT->new_command_req();
           if (cr->init(drive, window, true, &buf[cp])) {
