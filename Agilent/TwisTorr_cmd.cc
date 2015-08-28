c79bae13c8f7dbc97b7be90026140f4cca157476
Norton Allen <allen@huarp.harvard.edu>
2015-08-28 11:59:01 -0400

    back off on verbosity

--- Agilent/TwisTorr_cmd.cc
+++ Agilent/TwisTorr_cmd.cc
@@ -70,14 +70,14 @@ int TwisTorr_cmd::ProcessData(int flag) {
 
 void TwisTorr::enqueue_poll_float(uint8_t drive, uint16_t window, float *ptr) {
   command_request *cr;
-  nl_error(MSG_DBG(2), "Enqueuing poll_float drive:%d window:%d", drive, window);
+  // nl_error(MSG_DBG(2), "Enqueuing poll_float drive:%d window:%d", drive, window);
   cr = new_command_req();
-  nl_error(MSG_DBG(2), "Have cr, ready to init");
+  // nl_error(MSG_DBG(2), "Have cr, ready to init");
   if (cr->init(drive, window, true)) {
     nl_error(MSG_DBG(2), "command_request::init failed");
     free_command(cr);
   } else {
-    nl_error(MSG_DBG(2), "command_request::init succeeded");
+    // nl_error(MSG_DBG(2), "command_request::init succeeded");
     cr->set_fl_ptr(ptr);
     enqueue_poll(cr);
     nl_error(MSG_DBG(1), "Enqueued poll_float drive:%d window:%d", drive, window);
