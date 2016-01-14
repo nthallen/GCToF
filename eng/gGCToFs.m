3b33dc9f61b8f2e38ce64adf2a8ec1c72407452b
Norton Allen <allen@huarp.harvard.edu>
2016-01-13 20:57:01 -0500

    Sonic engineering files

---
+++ eng/gGCToFs.m
@@ -0,0 +1,5 @@
+function fig = gGCToFs(varargin);
+% gGCToFs(...)
+% Sonic 1
+ffig = ne_group(varargin,'Sonic 1','pGCToFsw','pGCToFss','pGCToFsn','pGCToFsstatus');
+if nargout > 0 fig = ffig; end
---
+++ eng/gGCToFsonic2.m
@@ -0,0 +1,5 @@
+function fig = gGCToFsonic2(varargin);
+% gGCToFsonic2(...)
+% Sonic 2
+ffig = ne_group(varargin,'Sonic 2','pGCToFsonic2w','pGCToFsonic2s','pGCToFsonic2n','pGCToFsonic2status');
+if nargout > 0 fig = ffig; end
