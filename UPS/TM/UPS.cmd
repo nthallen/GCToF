54ce16356f23de65b0506bf82f579213d62301ea
Norton Allen <allen@huarp.harvard.edu>
2016-02-02 19:14:50 -0800

    Get T and S commands correct, QSK1 status

--- UPS/TM/UPS.cmd
+++ UPS/TM/UPS.cmd
@@ -1,11 +1,27 @@
 %INTERFACE <UPS>
 
 &command
-  : UPS Test for %d (Enter time in minutes) minutes *
-      { if_UPS.Turf("T%d\n", $4); }
-  : UPS &shutdown * { if_UPS.Turf("S%d\n", $2); }
+  : UPS Test for &minutes *
+      { int mins = $4;
+	if (mins < 1) mins = 1;
+	if (mins > 99) mins = 99;
+	if_UPS.Turf("T%02d\n", mins);
+      }
+  : UPS Test for &seconds *
+      { int tens = $4/6;
+	if (tens < 2) tens = 2;
+	if (tens > 9) {
+	  int mins = $4/60;
+	  if (mins < 1) mins = 1;
+	  if (mins > 99) mins = 00;
+	  if_UPS.Turf("T%02d\n", mins);
+	} else {
+	  if_UPS.Turf("T.%d\n", tens);
+	}
+      }
+  : UPS &shutdown * { if_UPS.Turf("S%02d\n", $2); }
   : UPS &shutdown and Restore after %d (Enter time in minutes) minutes *
-      { if_UPS.Turf("%S%dR%d\n", $2, $6); }
+      { if_UPS.Turf("S%02dR%04d\n", $2, $6); }
   : UPS Cancel Shutdown * { if_UPS.Turf("CS\n"); }
   : UPS Cancel Test * { if_UPS.Turf("CT\n"); }
   : UPS Socket 1 On * { if_UPS.Turf("KN\n"); }
@@ -23,6 +39,12 @@
   : UPS Quit * { if_UPS.Turf("X\n"); }
   ;
 
+&minutes <int>
+  : minutes %d (Enter time in minutes) { $0 = $2; }
+  ;
+&seconds <int>
+  : seconds %d (Enter time in seconds) { $0 = $2; }
+  ;
 &shutdown <int>
   : Shutdown in %d (Enter time in minutes) minutes { $0 = $3; }
   ;
