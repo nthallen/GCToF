696eadc537fd77cfb136e0c81e28aff27999efc6
Norton Allen <allen@huarp.harvard.edu>
2016-06-11 11:41:41 -0400

    Ensure everything gets displayed and extracted

--- eng/gGCToFdsd.m
+++ eng/gGCToFdsd.m
@@ -1,5 +1,5 @@
 function fig = gGCToFdsd(varargin);
 % gGCToFdsd(...)
 % DS Daq
-ffig = ne_group(varargin,'DS Daq','pGCToFdsds');
+ffig = ne_group(varargin,'DS Daq','pGCToFdsds','pGCToFdsdstatus');
 if nargout > 0 fig = ffig; end
--- eng/gGCToFgbh.m
+++ eng/gGCToFgbh.m
@@ -1,5 +1,5 @@
 function fig = gGCToFgbh(varargin);
 % gGCToFgbh(...)
 % GB Horiba
-ffig = ne_group(varargin,'GB Horiba','pGCToFgbhf','pGCToFgbhis','pGCToFgbhdft','pGCToFgbhcp','pGCToFgbhs','pGCToFgbhstale');
+ffig = ne_group(varargin,'GB Horiba','pGCToFgbhf','pGCToFgbhs','pGCToFgbhis','pGCToFgbhdft','pGCToFgbhcp','pGCToFgbhstale');
 if nargout > 0 fig = ffig; end
