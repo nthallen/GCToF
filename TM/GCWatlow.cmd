72a1c7d603bd2e50c997421ec65f5e6862f67922
Norton Allen <allen@huarp.harvard.edu>
2016-06-16 13:25:23 -0400

    Support for named profiles

--- TM/GCWatlow.cmd
+++ TM/GCWatlow.cmd
@@ -11,8 +11,11 @@
   : GCWatlow F4 Set &GCWatlow_F4 %f (Enter Temperature in Celcius) * {
       if_GCWatlow.Turf("F%d:%d\n", $4, (int)floor($5*10 + 0.5));
     }
-  : GCWatlow F4 &GCWatlow_F4_PC Profile * {
-      if_GCWatlow.Turf("F%d:1\n", $3);
+  : GCWatlow F4 Profile &GCWatlow_F4_PC * {
+      if_GCWatlow.Turf("F%d:1\n", $4);
+    }
+  : GCWatlow F4 Profile Start &GCWatlow_F4_FN * {
+      if_GCWatlow.Turf("F5:%d\n", $5);
     }
   ;
 
@@ -25,8 +28,12 @@
   : GC Spare F4 Temperature 1  { $0 = 2; }
   ;
 &GCWatlow_F4_PC <int>
-  : Start { $0 = 5; }
   : Suspend { $0 = 6; }
   : Resume { $0 = 3; }
   : Terminate { $0 = 4; }
+  ;
+&GCWatlow_F4_FN <int>
+  : File Number 1 { $0 = 1; }
+  : File Number 2 { $0 = 2; }
+  : File Number 3 { $0 = 3; }
   ;
