13ef57a2226cc71579abdf513c337d5d4781af17
Norton Allen <allen@huarp.harvard.edu>
2016-07-04 14:07:59 -0400

    Remove redundant sonic genui defs, obsolete Horiba plots

--- eng/gGCToFh.m
+++
@@ -1,5 +0,0 @@
-function fig = gGCToFh(varargin);
-% gGCToFh(...)
-% Horiba
-ffig = ne_group(varargin,'Horiba','pGCToFhd','pGCToFhis','pGCToFhf','pGCToFhp','pGCToFhs','pGCToFhstale');
-if nargout > 0 fig = ffig; end
--- eng/gGCToFsonic2.m
+++ eng/gGCToFsonic2.m
@@ -1,5 +1,5 @@
 function fig = gGCToFsonic2(varargin);
 % gGCToFsonic2(...)
 % Sonic 2
-ffig = ne_group(varargin,'Sonic 2','pGCToFsonic2w','pGCToFsonic2s','pGCToFsonic2n','pGCToFsonic2status','pGCToFsonic2t','pGCToFsonic2stale');
+ffig = ne_group(varargin,'Sonic 2','pGCToFsonic2w','pGCToFsonic2s','pGCToFsonic2t','pGCToFsonic2n','pGCToFsonic2status','pGCToFsonic2stale');
 if nargout > 0 fig = ffig; end
